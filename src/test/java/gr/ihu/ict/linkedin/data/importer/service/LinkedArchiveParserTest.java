e19bed1f3ef5b8e6c050bd39d883b1c627822c27
Iordanis Kostelidis <ikostelidis@kostelidis.dev>
2025-04-05 19:15:37 +0300

    add more tests

--- src/test/java/gr/ihu/ict/linkedin/data/importer/service/LinkedArchiveParserTest.java
+++ src/test/java/gr/ihu/ict/linkedin/data/importer/service/LinkedArchiveParserTest.java
@@ -2,6 +2,7 @@ package gr.ihu.ict.linkedin.data.importer.service;
 
 import gr.ihu.ict.linkedin.data.importer.AbstractTest;
 import gr.ihu.ict.linkedin.data.importer.model.LinkedInData;
+import gr.ihu.ict.linkedin.data.importer.service.csv.impl.*;
 import io.vavr.control.Try;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,18 +10,34 @@ import org.junit.Test;
 
 public class LinkedArchiveParserTest extends AbstractTest {
 
-    private LinkedInDataImporter linkedArchiveParser;
+    private LinkedInDataImporter linkedArchiveParserAuto;
+    private LinkedInDataImporter linkedArchiveParserCustom;
 
     @Before
     public void setUp() throws Exception {
-        this.linkedArchiveParser = new LinkedInDataImporterImpl();
+        this.linkedArchiveParserAuto = new LinkedInDataImporterImpl();
+        this.linkedArchiveParserCustom = new LinkedInDataImporterImpl(
+                new CertificationParserImpl(),
+                new EducationParserImpl(),
+                new LanguageParserImpl(),
+                new PositionParserImpl(),
+                new ProfileParserImpl(),
+                new ProjectParserImpl(),
+                new PublicationParserImpl(),
+                new SkillParserImpl()
+        );
     }
 
     @Test
     public void testParseZip() {
-        Try<LinkedInData> result = this.getFileFromResources("Basic_LinkedInDataExport_01-29-2022.zip")
-                .flatMap(file -> Try.of(() -> linkedArchiveParser.parseZip(file)));
+        Try<LinkedInData> resultAuto = this.getFileFromResources("Basic_LinkedInDataExport_01-29-2022.zip")
+                .flatMap(file -> Try.of(() -> linkedArchiveParserAuto.parseZip(file)));
 
-        Assert.assertTrue(result.isSuccess());
+        Assert.assertTrue(resultAuto.isSuccess());
+
+        Try<LinkedInData> resultCustom = this.getFileFromResources("Basic_LinkedInDataExport_01-29-2022.zip")
+                .flatMap(file -> Try.of(() -> linkedArchiveParserCustom.parseZip(file)));
+
+        Assert.assertTrue(resultCustom.isSuccess());
     }
 }
